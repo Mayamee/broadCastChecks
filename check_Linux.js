c4187c90ba2654b6a0651ba7316c7f2c6c7450c5
Mew <54446552+Mayamee@users.noreply.github.com>
2021-08-07 23:59:13 +0300

    Add files via upload

---
+++ check_Linux.js
@@ -0,0 +1,72 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const TCP_CHECK_COMMAND =
+  "tcpdump -i ens33 -c 10 host 192.168.0.255 -q -n 2> /dev/null | tee log.log";
+const createFile = (name) => {
+  execSync(`tee ${name}`);
+};
+const sortObj = (obj) => {
+  return Object.fromEntries(Object.entries(obj).sort(([, a], [, b]) => b - a));
+};
+//###################################// INIT
+
+createFile("stats.json");
+
+//###################################// INIT
+
+//###################################// LOOP
+while (true) {
+  let outOfExec = execSync(TCP_CHECK_COMMAND).toString();
+  let ipList = countIpToList(recognizeIP(outOfExec));
+  const currentStateOfMonitoring = fs.readFileSync("stats.json", "utf-8");
+  if (currentStateOfMonitoring) {
+    let currentListOfIP = JSON.parse(currentStateOfMonitoring);
+    let collizedObject = checkCollizions(ipList, currentListOfIP);
+    fs.writeFileSync(
+      "stats.json",
+      JSON.stringify(sortObj(collizedObject), null, "\t")
+    );
+  } else {
+    fs.writeFileSync("stats.json", JSON.stringify(ipList,null,"\t"));
+  }
+}
+//###################################// LOOP
+
+//###################################// FUNCTIONS
+
+function checkCollizions(obj1, obj2) {
+  for (const key in obj1) {
+    if (
+      Object.hasOwnProperty.call(obj1, key) &&
+      Object.hasOwnProperty.call(obj2, key)
+    ) {
+      obj1[key] += obj2[key];
+    }
+  }
+  return Object.assign(obj2, obj1);
+}
+
+function recognizeIP(data) {
+  return (
+    data
+      .split("\n")
+      .map((e) => e.match(/\d+\.\d+\.\d+\.\d+/))
+      // .filter((e) => e != "192.168.0.13")
+      .join("\n")
+  );
+}
+
+function countIpToList(log) {
+  const logArray = log.split("\n").filter((e) => e);
+  return mapArr(logArray);
+}
+
+function mapArr(arr) {
+  return arr.reduce((acc, cur) => {
+    if (!acc[cur]) acc[cur] = 1;
+    else acc[cur] += 1;
+    return acc;
+  }, {});
+}
+
+//###################################// FUNCTIONS
